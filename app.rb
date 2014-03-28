2ac2892749c5ae0e4f3ab630fec009fa05f86491
404pnf <404pnf@exmaple.org>
2014-03-28 09:08:32 +0800

    增加jquery.cookie插件

--- app.rb
+++ app.rb
@@ -2,46 +2,22 @@ require 'sinatra'
 require_relative 'lib/lib.rb'
 
 configure do
-  enable :sessions
+  #enable :sessions
 end
 
 get '/' do
-  session[:type] = '欢乐读书屋'
-  session[:cat] = '儿童绘本'
-  session[:age] = '1-3岁'
-  "try <a href='/content'>/content</a>"
+  erb :index
 end
 
 get '/content' do
-  redirect '/' if [session[:type], session[:cat], session[:age]].none?
-  type, cat, age = session[:type], session[:cat], session[:age]
-  # erb :content ,  :locals => {  content:    get_content(type, cat, age),
-  #                               tag:        menu_tag(type, cat, age),
-  #                               age:        menu_age(type, cat, age)
-  #                             }
-  r = get_content(type, cat, age)
-  "#{DB.size}"
-  "#{r}"
+  redirect '/' unless [request.cookies['type'], request.cookies['cat'], request.cookies['age']].all?
+  type, cat, age = request.cookies['type'], request.cookies['cat'], request.cookies['age']
+  erb :content ,  :locals => {  content:    get_content(type, cat, age),
+                                cat:        menu_cat(type, cat, age),
+                                age:        menu_age(type, cat, age)
+                              }
 end
 
-get '/session' do session.inspect end
-get '/session-clear' do session.clear; redirect '/session' end
-
-
-
-# 如果不使用sessino而是用url的restful方式
-#
-# get '/:type/:cat' do |type, cat|
-#   r = DB[type][:content].select { |e| e[:category] == cat }
-#   "#{r}"
-# end
-
-# get '/:type/:cat/:age' do |type, cat, age|
-#   r = DB[type][:content].select { |e| (e[:category] == cat) && (e[:age] == age)}
-#   content = get_content(type, cat, age)
-#   menu_tags = generate_menu(type, cat, age)
-#   menu_ages = DB[type][:age]
-#   "#{menu_tags}<hr>#{menu_ages}<hr>#{content}"
-#   #{}"#{type} #{cat} #{age}"
-#   session.inspect
-# end
+get '/session'        do [request.cookies["type"], request.cookies["cat"], request.cookies["age"]].join(', ') end
+get '/session-clear'  do session.clear; redirect '/session'   end
+get '/*'              do redirect '/'                         end
--- lib/lib.rb
+++ lib/lib.rb
@@ -4,44 +4,38 @@ require 'set'
 require 'csv'
 
 def make_db(file)
-  csv  =  CSV.table(file, converters: nil).map(&:to_hash)
-  #hsh    =  Hash.new { |h, k| h[k] = Set.new }
-  #tags =  csv.each_with_object(hsh) { |e, o|  o[e[:age]] << e[:category] }
-  #ages =  tags.keys
-  # {
-  #   #tag: tags,
-  #   #age: ages,
-  #   content: csv
-  # }
   CSV.table(file, converters: nil).map(&:to_hash)
 end
 
-# DB =   {
-#           'video'     =>  make_db('db/video.csv'),
-#           'bilingual' =>  make_db('db/bilingual.csv'),
-#           'nse'       =>  make_db('db/nse.csv'),
-#           'ebook'     =>  make_db('db/ebook.csv')
-#         }
-DB =   make_db('db/video.csv') + make_db('db/bilingual.csv') + make_db('db/nse.csv') + make_db('db/ebook.csv')
+DB =  ['bilingual.csv',
+       'nse.csv',
+       'video.csv',
+       'ebook.csv'
+      ].reduce([]) { |a , e| a + make_db("db/#{e}") }
+
+# 大分类没有出现在csv中，手工创建
 TYPE =  {
            早教乐乐园: ['亲子游乐园', '益智动画片', '欢乐音乐屋', '成长故事书', '英语小剧场'],
            欢乐读书屋: ['儿童绘本', '经典小说', '寓言故事', '童话故事', '双语阅读', '有声故事'],
            快乐小课堂: ['新标准英语', '跟我学英语', '彩虹英语']
         }
 
-def get_content(type, cat, age)
-  #DB[type][:content].select { |e| (e[:category] == cat) && (e[:age] == age)}
-  DB.select { |e| TYPE[type.intern].include?(e[:category]) && e[:age] == age }
+# 先根据大分类过滤出来所有内容。因为这个过滤比较麻烦，因此单独个方法出来
+def content_by_type(type)
+  DB.select { |e| TYPE[type.intern].include?(e[:category]) }
 end
 
-def menu_tag(type, cat, age)
-   DB[type][:content].select { |e| e[:age] == age }
-                     .reduce(Set.new) {|a, e| a << e[:category] }
-                     .to_a
+def get_content(type, cat, age)
+  content_by_type(type).select { |e| e[:age] == age }
+end
+
+def menu_cat(type, cat, age)
+   content_by_type(type) #.select { |e| e[:age] == age }
+                        .reduce(Set.new) {|a, e| a << e[:category] }
+                        .to_a
 end
 
 def menu_age(type, cat, age)
-    DB[type][:content].reduce(Set.new) {|a, e| a << e[:age] }
-                      .to_a
+   content_by_type(type).reduce(Set.new) {|a, e| a << e[:age] }
+                        .to_a
 end
-
